24b249ef90b1c4d4dba089c4eb6b6aa4edc29092
Marcus Juan <mj320997@gmail.com>
2026-09-05 22:01:17 -0300

    Questao 2 lista 3

--- Lista3/questao2.c
+++ Lista3/questao2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 void recebeVet(int vetor[], int tam);
+void inverteVet(int vetor[], int tam);
+void imprimirVet(int vet[], int tam);
 
 int main(){
 
@@ -9,11 +11,10 @@ int main(){
 
     int vet[qtdElementos];
     recebeVet(vet, qtdElementos);
+    
+    inverteVet(vet, qtdElementos);
 
-    for (int i = 0; i < qtdElementos; i++)
-    {
-        printf("%d ", vet[i]);
-    }
+    imprimirVet(vet, qtdElementos);
     
     
 }
@@ -25,4 +26,58 @@ void recebeVet(int vetor[], int tam){
         scanf("%d", &vetor[i]);
     }
     
+}
+
+void inverteVet(int vetor[], int tam){
+
+    int i = 0;
+    int zero = 1; //posição do zero no vetor
+    int somaVet = 0; //para verificar se tem zero no vetor
+
+    while (i < tam && zero != 0)
+    {
+        if(vetor[i] == 0){
+
+            zero = i;
+            i++;
+        }
+
+        i++;
+        somaVet++;
+    }
+    
+    for (int i = 0; i < zero / 2; i++)
+    {
+        int aux = vetor[i];
+        vetor[i] = vetor[zero - 1 - i];
+        vetor[zero - 1 - i] = aux ;
+    }
+
+    int primeiroElem = zero + 1; //posição do primeiro elemento depois do zero
+    int ultimoElemt = tam - 1;
+
+    for (int i = 0; i < (ultimoElemt  - primeiroElem + 1) / 2; i++)
+    {
+        int aux = vetor[primeiroElem + i];
+        vetor[primeiroElem + i] = vetor[ultimoElemt - i];
+        vetor[ultimoElemt - i] = aux ;
+    }
+
+    if(somaVet > 0){
+
+        for (int i = 0; i < zero / 2; i++)
+    {
+        int aux = vetor[i];
+        vetor[i] = vetor[zero - 1 - i];
+        vetor[zero - 1 - i] = aux ;
+    }
+    }
+}
+
+void imprimirVet(int vet[], int tam){
+
+    for (int i = 0; i < tam; i++)
+    {
+        printf("%d ", vet[i]);
+    }
 }
